30d13bb74526c1ce7064300236a537176e95f5c1
LingnaC <che23173@uvg.edu.gt>
2024-07-18 12:42:11 -0600

    Problema 1

---
+++ src/Problema_1.kt
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*
+fun main() {
+    //val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    //println("Hello, " + name + "!")
+
+    //for (i in 1..5) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+    println("Ingresa un número decimal:")
+    val number = readLine()?.toIntOrNull()
+    if (number != null) {
+        val binary = binary(number)
+        println("El número binario es: $binary")
+    } else {
+        println("Entrada no válida")
+    }
+}
+
+fun binary(decimal: Int):
+        String{
+    return Integer.toBinaryString(decimal)
+
+}
+
